a51bdadd7b186f263fd5d4a87695cdaa2ddf3cb6
Travis Swift <travis.arthur.swift@gmail.com>
2022-03-17 17:14:17 -0500

    added editable text fields

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 $(document).ready(
     function () {
         // Make a time block container for each hour
-        var firstHour = 9;
-        var lastHour = 17;
+        // var firstHour = 9;
+        // var lastHour = 17;
+
+        // window.location.reload()
 
         var currentHour = moment().hour();
         console.log(currentHour);
 
-        for (var i = firstHour; i <= lastHour; i++) {
+        for (var i = 9; i <= 17; i++) {
             // create a new element for the time block
-            var newTimeBlock = document.createElement("div");
+            // var newTimeBlock = document.createElement("div");
 
-            newTimeBlock.innerHTML = "<p>" + moment().hour(i).format("ha") + "</p>";
+            //  newTimeBlock.innerHTML = "<p>" + moment().hour(i).format("ha") + "</p>";
 
             // Apply the timeblock class to the element
             //  newTimeBlock.innerHTML = i + "am";
-            newTimeBlock.classList.add("timeblock");
+            //   newTimeBlock.classList.add("timeblock");
+
+            // Put a erfernce to the matching timelock
+            var newTimeBlock = document.getElementById(i);
 
             // decide which additiona class to apply based on the the current time
             if (i < currentHour) {
@@ -27,28 +32,50 @@ $(document).ready(
                 newTimeBlock.classList.add("future");
             }
 
+// LOADING GOES HERE
+
             // test that element is selected
-            newTimeBlock.setAttribute("onclick","alert('blah'), inputText;");  
-        
-
-            $(".container").append(newTimeBlock);
-
-
-            /* var createTask = function(timeblock) {
-            // edit time block
-            var timeblock = $(addEventListener(onclick);
-            console.log("clicked")
-        } // */
-
-
-            // On mouse-click, edit text
-
-            /* function myFunction() {
-               document.getElementById("timeblock").click(); 
-             }  */
+            //   newTimeBlock.setAttribute("onclick", "alert('blah'), inputText;");
 
 
         }
     }
 
 );
+
+    $(document).on("click", "p.note", function(){
+        var newInput = document.createElement("input");
+        newInput.type = "text";
+        newInput.classList.add("note");
+        newInput.value = $(this).html();
+
+        $(this).replaceWith(newInput);
+        newInput.focus();
+    }
+    );
+
+// $("p.note").click(
+//     function () {
+//         var newElement = document.createElement("input");
+//         newElement.type = "text";
+//         newElement.classList.add("note");
+//         newElement.value = $(this).html();
+//         newElement
+//             .addEventListener("blur");
+//         $(this).replaceWith(newElement);
+//         newInput.focu();
+//     }
+// );
+
+$(document).on("blur", "input.note", function(){
+// create a <p> element
+        var newParagraph = document.createElement("p");
+        newParagraph.classList.add("note");
+        newParagraph.innerHTML = $(this).val();
+
+        $(this).replaceWith(newParagraph);
+        
+        //save goes here
+    } 
+);
+
